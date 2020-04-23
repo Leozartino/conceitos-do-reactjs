9d15a59ff604dede6000893e3ca4ad8e93b8b2b0
Leozartino <leozartino@gmail.com>
2020-04-22 23:54:52 -0300

    feat(App.js): add logica inical

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      id: "1",
+      title: "Novo repositorio ",
+      url: "https://github.com/Leozartino/jogo-da-velha.git",
+      techs: ["CSS", "Javascript", "HTML"],
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    <>
+      <h1>Reposiorios</h1>
+      <div>
+        <ul data-testid="repository-list">
+          {repositories.map((repository) => (
+            <li key={repository.id}>{repository.title}</li>
+          ))}
+        </ul>
+        <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+    </>
   );
 }
 
